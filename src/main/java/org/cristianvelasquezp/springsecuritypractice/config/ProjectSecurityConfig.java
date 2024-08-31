3fd7027423224584a4b9848b545e731cf214fd0b
Cristian Velasquez <cristianvelasquezp@gmail.com>
2024-08-31 12:31:20 -0500

    implemented custom AuthenticationProvider

--- src/main/java/org/cristianvelasquezp/springsecuritypractice/config/ProjectSecurityConfig.java
+++ src/main/java/org/cristianvelasquezp/springsecuritypractice/config/ProjectSecurityConfig.java
@@ -2,6 +2,7 @@ package org.cristianvelasquezp.springsecuritypractice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,11 +11,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class ProjectSecurityConfig {
 
+
+    private final AuthenticationProvider authenticationProvider;
+
+    public ProjectSecurityConfig(AuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
+                .authenticationProvider(authenticationProvider)
                 .authorizeHttpRequests(authorizeRequests ->
                 authorizeRequests
                         .anyRequest().permitAll()
---
+++ src/main/java/org/cristianvelasquezp/springsecuritypractice/config/SSPracticeAuthenticationProvider.java
@@ -0,0 +1,45 @@
+package org.cristianvelasquezp.springsecuritypractice.config;
+
+import jdk.jfr.Category;
+import lombok.AllArgsConstructor;
+import org.cristianvelasquezp.springsecuritypractice.repositories.UserRepository;
+import org.hibernate.annotations.Comment;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class SSPracticeAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+        if (passwordEncoder.matches(password, userDetails.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(
+                    username,
+                    password,
+                    userDetails.getAuthorities());
+        } else {
+            throw new BadCredentialsException("Authentication failed");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
